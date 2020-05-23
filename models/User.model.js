1ea07f2d129d95ca6ae2f2351c9bbfe6b78d3b9e
veroca88 <veronicadonoso1506@gmail.com>
2020-05-22 22:40:52 -0400

    Create user model

---
+++ models/User.model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      required: [true, 'Username must be unique!'],
+      unique: true,
+    },
+    passwordHash: {
+      type: String,
+    }
+  }
+);
+
+module.exports = model('User', userSchema);
